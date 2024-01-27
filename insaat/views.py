24c6f055d9e3aeee75cfcfa8cbcbd4f5df78e83e
fatihkurtl <kurtf061@gmail.com>
2024-01-27 23:58:56 +0300

    revize edildi

--- insaat/views.py
+++ insaat/views.py
@@ -28,22 +28,25 @@ def index(request):
     logo_images = Logo.objects.all()
     about_text = AboutText.objects.first()
     wyh_us = WhyUs.objects.first()
-    return render(request, 'index.html', {'communication_info': communication_info,
-                                          'slide_images': slide_images,
-                                          'service_images': service_images,
-                                          'project_images': project_images,
-                                          'testimonials': testimonials,
-                                          "logo_images": logo_images,
-                                          'about_text': about_text,
-                                          'why_us': wyh_us,
-                                          'quote': quote,
-                                          'categories': categories,
-                                          'products': products,
-                                          'floor_categories': floor_categories,
-                                        #   'cat_floor': cat_floor,
-                                        #   'floor': floor,
-                                          
-                                          })
+    
+    data = {
+            'communication_info': communication_info,
+            'slide_images': slide_images,
+            'service_images': service_images,
+            'project_images': project_images,
+            'testimonials': testimonials,
+            'logo_images': logo_images,
+            'about_text': about_text,
+            'why_us': wyh_us,
+            'quote': quote,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            # 'cat_floor': cat_floor,
+            # 'floor': floor,                                     
+        }
+    
+    return render(request, 'index.html', data)
 
 
 
@@ -52,89 +55,123 @@ def about(request):
     logo_images = Logo.objects.all()
     about_text = AboutText.objects.first()
     wyh_us = WhyUs.objects.get()
-    return render(request, "about.html", {'communication_info': communication_info,
-                                          "logo_images": logo_images,
-                                          'about_text': about_text,
-                                          'why_us': wyh_us,
-                                          'categories': categories,
-                                          'products': products,
-                                          'floor_categories': floor_categories,
-                                          })
+    
+    data = {
+            'communication_info': communication_info,
+            'logo_images': logo_images,
+            'about_text': about_text,
+            'why_us': wyh_us,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+        }
+    
+    return render(request, "about.html", data)
 
 def service(request):
     service_images = ServiceImage.objects.all()
     testimonials = Testimonial.objects.all()
     logo_images = Logo.objects.all()
     quote = FreeQuote.objects.get()
-    return render(request, "service.html", {'communication_info': communication_info,
-                                            'service_images': service_images,
-                                            'testimonials': testimonials,
-                                            "logo_images": logo_images,
-                                            'quote': quote,
-                                            'categories': categories,
-                                            'products': products,
-                                            'floor_categories': floor_categories,
-                                            })
+    
+    data = {
+            'communication_info': communication_info,
+            'service_images': service_images,
+            'testimonials': testimonials,
+            'logo_images': logo_images,
+            'quote': quote,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            }
+    
+    return render(request, "service.html", data)
 
 def project(request):
     project_images = ProjectImage.objects.all()
     logo_images = Logo.objects.all()
-    return render(request, "project.html", {'communication_info': communication_info,
-                                            'project_images': project_images,
-                                            "logo_images": logo_images,
-                                            'categories': categories,
-                                            'products': products,
-                                            'floor_categories': floor_categories,})
+    
+    data = {
+            'communication_info': communication_info,
+            'project_images': project_images,
+            'logo_images': logo_images,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            }
+    
+    return render(request, "project.html", data)
 
 def feature(request):
     logo_images = Logo.objects.all()
     wyh_us = WhyUs.objects.get()
-    return render(request, "feature.html", {'communication_info': communication_info,
-                                            "logo_images": logo_images,
-                                            'why_us': wyh_us,
-                                            'categories': categories,
-                                            'products': products,
-                                            'floor_categories': floor_categories,
-                                            })
+    
+    data = {
+            'communication_info': communication_info,
+            'logo_images': logo_images,
+            'why_us': wyh_us,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            }
+    
+    return render(request, "feature.html", data)
 
 def freequote(request):
     logo_images = Logo.objects.all()
     quote = FreeQuote.objects.get()
-    return render(request, "freequote.html", {'communication_info': communication_info,
-                                              "logo_images": logo_images,
-                                              'quote': quote,
-                                            'categories': categories,
-                                            'products': products,
-                                            'floor_categories': floor_categories,})
+    
+    data = {
+            'communication_info': communication_info,
+            'logo_images': logo_images,
+            'quote': quote,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            }
+    
+    return render(request, "freequote.html", data)
 
 
 
 def testimonial(request):
     testimonials = Testimonial.objects.all()
     logo_images = Logo.objects.all()
-    return render(request, "testimonial.html", {'communication_info': communication_info,
-                                                'testimonials': testimonials,
-                                                "logo_images": logo_images,
-                                                'categories': categories,
-                                                'products': products,
-                                                'floor_categories': floor_categories,})
+    
+    data = {
+            'communication_info': communication_info,
+            'testimonials': testimonials,
+            'logo_images': logo_images,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,
+            }
+    
+    return render(request, "testimonial.html", data)
 
 
 def contact(request):
     logo_images = Logo.objects.all()
-    return render(request, "contact.html", {'communication_info': communication_info,
-                                            "logo_images": logo_images,
-                                            'categories': categories,
-                                            'products': products,
-                                            'floor_categories': floor_categories,   
-                                            })
+    
+    data = {
+            'communication_info': communication_info,
+            'logo_images': logo_images,
+            'categories': categories,
+            'products': products,
+            'floor_categories': floor_categories,   
+            }
+    
+    return render(request, "contact.html", data)
     
 def error(request, exception):
     logo_images = Logo.objects.all()
     
-    return render(request, "error.html", {'communication_info': communication_info,
-                                          'logo_images': logo_images},
-                  status=404 )
+    data = {
+            'communication_info': communication_info,
+            'logo_images': logo_images
+            }
+    
+    return render(request, "error.html", data, status=404 )
     
 
 
--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gr)bit4k2dt6+(w0ov&px)n_s@94cc!3h_m9o69xp(l_n=y4le'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

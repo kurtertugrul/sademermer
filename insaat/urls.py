1d0a72d81af92c15e48eb6979b11d31363fbac3c
Ertugrul <ertugrulkurttt@gmail.com>
2024-01-30 14:08:18 +0300

    degisti

--- insaat/urls.py
+++ insaat/urls.py
@@ -7,14 +7,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('service', views.service, name='service'),
-    path('project', views.project, name='project'),
-    path('feature', views.feature, name='feature'),
-    path('freequote', views.freequote, name='freequote'),
-    path('testimonial', views.testimonial, name='testimonial'),
+    path('hakkimizda', views.about, name='about'),
+    path('servisler', views.service, name='service'),
+    path('projeler', views.project, name='project'),
+    path('neden_biz', views.feature, name='feature'),
+    path('fiyat_teklifi', views.freequote, name='freequote'),
+    path('referanslar', views.testimonial, name='testimonial'),
     path('error', views.error, name='error'),
-    path('contact', views.contact, name='contact'),
+    path('bize_ulasin', views.contact, name='contact'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
